1c392bc34cebe881f79ef9edd27b597f1caad821
alejandrotoro18 <alejandrotoro18@gmail.com>
2022-08-17 15:57:58 -0300

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ListEmpleadosComponent } from '../../components/list-empleados/list-empleados.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,],
-  declarations: [ AppComponent, HelloComponent,ListEmpleadosComponent],
+  declarations: [ AppComponent, HelloComponent,ListEmpleadosComponent,NavbarComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
